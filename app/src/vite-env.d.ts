9ee499718940ac6d5021b94634ccb066b16af642
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-05-22 12:31:01 -0400

    refactor: add alchemy api variable

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,9 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly ALCHEMY_ID: string
-  readonly ALCHEMY_SEPOLIA_URL: string;
-  readonly MNEMONIC: string;
+  readonly VITE_APP_ALCHEMY_API_KEY: string
 }
 
 interface ImportMeta {
