3fdf7c1185de1c9ac127f530ebc567a5c1a3b84f
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-05-22 12:26:41 -0400

    refactor: trim hardhat redundancies

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -24,39 +24,20 @@ const config: HardhatUserConfig = {
     },
     sepolia: {
       url: process.env.ALCHEMY_SEPOLIA_URL,
-      accounts: accounts,
+      accounts,
+    },
+    mumbai: {
+      url: process.env.ALCHEMY_MUMBAI_URL,
+      accounts,
     },
   },
   etherscan: {
-    customChains: [
-      {
-        network: "chiado",
-        chainId: 10200,
-        urls: {
-          //Blockscout
-          apiURL: "https://blockscout.com/gnosis/chiado/api",
-          browserURL: "https://blockscout.com/gnosis/chiado",
-        },
-      },
-      {
-        network: "gnosis",
-        chainId: 100,
-        urls: {
-          // 3) Select to what explorer verify the contracts
-          // Gnosisscan
-          apiURL: "https://api.gnosisscan.io/api",
-          browserURL: "https://gnosisscan.io/",
-          // Blockscout
-          //apiURL: "https://blockscout.com/xdai/mainnet/api",
-          //browserURL: "https://blockscout.com/xdai/mainnet",
-        },
-      },
-    ],
     apiKey: {
       //4) Insert your Gnosisscan API key
       //blockscout explorer verification does not require keys
       chiado: "your key",
       gnosis: process.env.GNOSISSCAN_API_KEY || "",
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY || "",
     },
   },
 };
